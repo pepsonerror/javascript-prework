90c7892842ca1307afcdf19351be01bcaf4cfa8a
Paweł Przytuła <pepsonerror@gmail.com>
2019-07-26 00:20:11 +0200

    add choise procedure in script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to ' + computerMove + ', to wygrywasz!');
+const possibleChoise = ['kamień','papier','nożyce'];
+const errorChoise = 'nieznany ruch';
+const prefixMessage = 'Twój ruch to: ';
+
+let playerInput = prompt(`Wybierz swój ruch! 1: ${possibleChoise[0]}, 2: ${possibleChoise[1]}, 3: ${possibleChoise[2]}.`);
+console.log('Gracz wpisał: ' + playerInput);
+
+switch(playerInput) {
+    case '1':
+    case '2':
+    case '3':
+        printMessage(prefixMessage + possibleChoise[playerInput - 1]);
+        break;        
+    default:
+        printMessage(prefixMessage + errorChoise);
+}
+
