0dfd1c059b01b5afc2e51e1d0ca931af82963600
Paweł Przytuła <pepsonerror@gmail.com>
2019-07-26 13:41:33 +0200

    DRY - 1

--- js/script.js
+++ js/script.js
@@ -3,23 +3,36 @@ const errorChoise = 'nieznany ruch';
 const messages = ['Twój ruch to: ','Komputer wylosował: '];
 
 let playerInput = prompt(`Wybierz swój ruch! 1: ${possibleChoise[0]}, 2: ${possibleChoise[1]}, 3: ${possibleChoise[2]}.`);
-let computerChoise = possibleChoise[Math.floor(Math.random() * 3)]; // possible indexs: 0..2
-console.log('Gracz wpisał: ' + playerInput);
+let computerChoise = possibleChoise[Math.floor(Math.random() * 3)];
 
 switch(playerInput) {
     case '1':
     case '2':
     case '3':
-        printMessage(   '<p>' + messages[0] + possibleChoise[playerInput - 1] + '.</p>' +
-                        '<p>' + messages[1] + computerChoise + '.</p>' + 
-                        '<p>' + (   computerChoise === possibleChoise[0] &&
-                                    possibleChoise[playerInput - 1] === possibleChoise[1]? 
-                                    'Ty wygrywasz!' : 
-                                    'Komputer wygrał!'
-                                ) + '</p>'
+        const playerChoiseText= getTextOfChoiseNatural(playerInput);
+        printMessage(   messageWithTagPEndDot(  messages[0] + playerChoiseText) +
+                        messageWithTagPEndDot(  messages[1] + computerChoise ) +
+                        messageWithTagPEndDot(  computerChoise === possibleChoise[0] &&
+                                                playerChoiseText === possibleChoise[1]? 
+                                                'Ty wygrywasz!' : 
+                                                'Komputer wygrał!'
+                                             )
                     );
         break;        
-    default:
+    default:1
         printMessage(messages[0] + errorChoise);
 }
 
+/* tylko takie funkcje przyszły mi na myśl, bo nie bardzo wiem co można by jeszcze uprościć  DRY */
+function getTextOfChoiseNatural(index){
+    try{
+        return possibleChoise[index - 1]
+    }catch(error){
+        console.log('Error!: ' + error);        
+        return '?!';
+    }
+}
+
+function messageWithTagPEndDot(textMessage){
+    return '<p>' + textMessage + '.</p>';
+}
